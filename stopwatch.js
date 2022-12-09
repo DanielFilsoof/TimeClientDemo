84a3b19a7140b1b30af1f69500db16afac237342
daniel <dfm@stil.dk>
2022-12-09 09:01:46 +0100

    adjust logic for stopwatch.js

--- stopwatch.js
+++ stopwatch.js
@@ -1,27 +1,35 @@
 window.onload = function () {
-    var appendTens = document.getElementById("tens")
     var appendSeconds = document.getElementById("seconds")
     var buttonStart = document.getElementById('button-start');
     var buttonStop = document.getElementById('button-stop');
     var buttonReset = document.getElementById('button-reset');
     var testafviklerGif = document.getElementById('testafvikling-gif');
     var systemUdregnetTid = document.getElementById('tid-input');
-    var currentSeconds = 0;
     var intervalId;
 
+    var startTime;
+    var elapsedTime;
+
     buttonStart.onclick = function() {
-        intervalId = setInterval(incrementSecond, 1000);
+        // intervalId = setInterval(incrementSecond, 500);
         testafviklerGif.classList.remove('hide');
         buttonStop.classList.remove('disabled');
         buttonStart.classList.add('disabled');
         buttonReset.classList.add('disabled');
 
+        startTime = Date.now(); // get the current time
+        elapsedTime = 0;
+        intervalId = setInterval(updateTime, 1000); // update the time every second
+
         window.sessionStorage.setItem('start-tid', new Date());
     }
 
-    function incrementSecond() {
-        currentSeconds++;
-        appendSeconds.innerHTML = currentSeconds;
+    function updateTime() {
+        elapsedTime = Date.now() - startTime;
+        let seconds = Math.floor(elapsedTime / 1000); // convert to seconds
+
+        // display the elapsed time
+        appendSeconds.innerHTML = seconds + " sekunder";
     }
 
     buttonStop.onclick = function() {
@@ -42,8 +50,8 @@ window.onload = function () {
 
     buttonReset.onclick = function() {
         buttonStart.classList.remove('disabled');
-        currentSeconds = 0;
-        appendSeconds.innerHTML = currentSeconds;
+        elapsedTime = '';
+        appendSeconds.innerHTML = elapsedTime;
         buttonStop.classList.add('disabled');
     }
 }
